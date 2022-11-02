1ca28f5355ecda62959fd419c6037f1bb4e245f5
Aingaran Thirunadarajah <aingaran.thirunadarajah@telus.com>
2022-11-02 14:43:00 -0400

    finished kata sum

---
+++ conditional-sums.js
@@ -0,0 +1,11 @@
+const conditionalSum = function(values, condition) {
+  let output;
+
+  for (let i=0; i<= values.length; i++)
+  {
+    console.log(values[i])
+  }
+
+};
+
+console.log(conditionalSum([1, 2, 3, 4, 5], "even"));
--- sum-the-largest-numbers.js
+++ sum-the-largest-numbers.js
@@ -1,7 +1,30 @@
-const sumLargestNumbers = function(data) {
-  // Put your solution here
+let BigNumber1;
+let BigNumber2;
+
+const sumLargestNumbers = function (numbers) {
+  let sum;
+
+  if (numbers[0] > numbers[1]) {
+    BigNumber1 = numbers[0];
+    BigNumber2 = numbers[1];
+  }
+  else {
+    BigNumber1 = numbers[1];
+    BigNumber2 = numbers[0];
+  }
+
+  for (let i = 2; i < numbers.length; i++) {
+    if (numbers[i] > BigNumber1) {
+      BigNumber2 = BigNumber1;
+      BigNumber1 = numbers[i];
+    }
+  }
+
+  sum = BigNumber1 + BigNumber2;
+
+  return sum;
 };
 
-// console.log(sumLargestNumbers([1, 10]));
+console.log(sumLargestNumbers([1, 10]));
 // console.log(sumLargestNumbers([1, 2, 3]));
-console.log(sumLargestNumbers([10, 4, 34, 6, 92, 2]));
+// console.log(sumLargestNumbers([10, 4, 34, 6, 92, 2]));
