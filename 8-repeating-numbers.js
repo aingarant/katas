0bb5f72a3cb89863dbb87fe1f8906355697be045
Aingaran Thirunadarajah <aingaran.thirunadarajah@telus.com>
2022-11-04 16:52:20 -0400

    completed repeating numbers kata

--- 8-repeating-numbers.js
+++ 8-repeating-numbers.js
@@ -1,25 +1,17 @@
 const repeatNumbers = (numbers) => {
   result = "";
 
-
-
   for (let i = 0; i < numbers.length; i++) {
-
-
     let j = 0;
     while (j < numbers[i][1]) {
-      result += numbers[i][0]
-      j++
+      result += numbers[i][0];
+      j++;
     }
-
+    if (numbers.length > 1 && i < numbers.length-1)
     result += ", ";
-
-
-
   }
-
-
-
   return result;
-}
+};
+console.log(repeatNumbers([[1, 10]]));
+console.log(repeatNumbers([[1, 2], [2, 3]]));
 console.log(repeatNumbers([[10, 4], [34, 6], [92, 2]]));
