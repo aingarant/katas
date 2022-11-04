05d92a8aa6342288689470cd14fd502b1ee0282c
Aingaran Thirunadarajah <aingaran.thirunadarajah@telus.com>
2022-11-04 16:10:18 -0400

    Completed Katas 14 Change Calulator

---
+++ 14-change-calculator.js
@@ -0,0 +1,41 @@
+const calculateChange = function (total, cash) {
+
+  const coins = [
+    { value: 2000, label: 'twentyDoollar' },
+    { value: 1000, label: 'tenDollar' },
+    { value: 500, label: 'fiveDollar' },
+    { value: 200, label: 'twoDollar' },
+    { value: 100, label: 'oneDollar' },
+    { value: 25, label: 'quarter' },
+    { value: 10, label: 'dime' },
+    { value: 5, label: 'nickel' },
+    { value: 1, label: 'penny' }
+  ]
+  let change = {};
+  let output = {}
+
+  // get change amount.
+  amount = cash - total;
+
+  for (let i = 0; amount > 0 && coins.length > i; i++) {
+
+    // get coin/bill
+    let value = coins[i].value;
+
+    // if change is bigger than the coin/bill
+    if (value <= amount) {
+
+      // find out how many coin/bill fit in amount.
+      change[value] = Math.floor(amount / value);
+
+      // add response to output object
+      output[coins[i].label] = change[value]
+
+      amount -= value * change[value]
+    }
+  }
+  return output;
+};
+
+
+console.log(calculateChange(501, 1000));
