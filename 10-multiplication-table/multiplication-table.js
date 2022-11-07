80fd68ca292902f5d2fdcaaadb71e8f1f12180ab
Aingaran Thirunadarajah <aingaran.thirunadarajah@telus.com>
2022-11-07 09:44:49 -0500

    finally finished multiplication table

--- 10-multiplication-table/multiplication-table.js
+++ 10-multiplication-table/multiplication-table.js
@@ -1,24 +1,15 @@
 const multiplicationTable = (maxValue) => {
   let x = "";
-  let y = "";
-  let a="";
-  for (let i = 0; i < maxValue; i++) {
-    {
-      a +=   i+1;
-      let j=0;
-      {
-        while (j < maxValue)
-        {
-          y += j * a;
-          console.log(y)
-          j++
-        }
-      }
+
+
+  for (let i = 1; i <= maxValue; i++) {
+    for (let j = 1; j <= maxValue; j++) {
+      x += i * j + " ";
     }
+    x += "\n";
   }
   return x;
 }
-
 console.log(multiplicationTable(1));
 console.log(multiplicationTable(5));
 console.log(multiplicationTable(10));
