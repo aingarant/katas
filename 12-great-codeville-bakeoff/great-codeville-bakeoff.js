572023c84c4788103cbe8bd04bc79918dc0db280
Aingaran Thirunadarajah <aingaran.thirunadarajah@telus.com>
2022-11-07 14:30:23 -0500

    update

--- 12-great-codeville-bakeoff/great-codeville-bakeoff.js
+++ 12-great-codeville-bakeoff/great-codeville-bakeoff.js
@@ -1,41 +1,31 @@
-const chooseRecipe = function(bakeryA, bakeryB, recipes) {
-  // Code here!
-}
-
-let bakeryA = ['saffron', 'eggs', 'tomato paste', 'coconut', 'custard'];
-let bakeryB = ['milk', 'butter', 'cream cheese'];
+let bakeryA = ["saffron", "eggs", "tomato paste", "coconut", "custard"];
+let bakeryB = ["milk", "butter", "cream cheese"];
 let recipes = [
     {
-        name: 'Coconut Sponge Cake',
-        ingredients: ['coconut', 'cake base']
+        name: "Coconut Sponge Cake",
+        ingredients: ["coconut", "cake base"],
     },
     {
-        name: 'Persian Cheesecake',
-        ingredients: ['saffron', 'cream cheese']
+        name: "Persian Cheesecake",
+        ingredients: ["saffron", "cream cheese"],
     },
     {
-        name: 'Custard Surprise',
-        ingredients: ['custard', 'ground beef']
-    }
+        name: "Custard Surprise",
+        ingredients: ["custard", "ground beef"],
+    },
 ];
 
-console.log(chooseRecipe(bakeryA, bakeryB, recipes));
+const chooseRecipe = (bakeryA, bakeryB, recipes) => {
+    const checkStock = (ingredients) => {
+      
 
-bakeryA = ['potatoes', 'bay leaf', 'raisins'];
-bakeryB = ['red bean', 'dijon mustard', 'apples'];
-recipes = [
-    {
-        name: 'Potato Ganache',
-        ingredients: ['potatoes', 'chocolate']
-    },
-    {
-        name: 'Sweet Fish',
-        ingredients: ['anchovies', 'honey']
-    },
-    {
-        name: "Nima's Famous Dijon Raisins",
-        ingredients: ['dijon mustard', 'raisins']
     }
-];
+
+    recipes.map((recipe) => {
+        return checkStock(recipe.ingredients)
+            ? console.log(recipe.name)
+            : console.log("No recipes");
+    });
+};
 
 console.log(chooseRecipe(bakeryA, bakeryB, recipes));
